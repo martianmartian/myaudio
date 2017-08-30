3a9cb9d1f0545b8e05a64c0a9902c3ece61f34ca
martian2049 <ginx.qx@gmail.com>
2017-08-30 09:13:33 +0800

    Initial Commit

---
+++ myaudio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myaudio
+//
+//  Created by martian2049 on 8/30/17.
+//  Copyright © 2017 martian2049. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
