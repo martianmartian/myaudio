1952f11661586d0192555540da4f8c10895cc2a0
martian2049 <ginx.qx@gmail.com>
2017-09-18 21:08:40 +0800

    second controller has view as well

--- myaudio/SecondViewController.swift
+++ myaudio/SecondViewController.swift
@@ -9,13 +9,36 @@
 
 import UIKit
 
-class SecondViewController: UIViewController{
+class SecondViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
+    let arr=["1","2","3","4","5"]
+    let indexArr = ["A","B","C","D","E"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return indexArr[section]
+    }
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return indexArr
+    }
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("once")
+        //let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier:"itemCell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell") as! itemCell
+        cell.textLabel?.text = arr[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(arr[indexPath.row])
+        
+    }
     
     
 }
---
+++ myaudio/itemCell.swift
@@ -0,0 +1,18 @@
+//
+//  itemCell.swift
+//  myaudio
+//
+//  Created by martian2049 on 9/18/17.
+//  Copyright © 2017 martian2049. All rights reserved.
+//
+
+
+import UIKit
+
+class itemCell: UITableViewCell {
+    
+
+    @IBOutlet weak var itemCell: UILabel!
+    
+    
+}
